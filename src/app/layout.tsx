8ea15e6303d0c83a2587c2681ed40f70b4e88386
Irfan-majumder <irfanmajumder07@gmail.com>
2026-06-02 10:38:00 +0530

    added clerk auth + middleware as proxy.ts + .env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -24,6 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${inter.variable} ${geistMono.variable} antialiased`}
@@ -33,5 +35,6 @@ export default function RootLayout({
         <Toaster />
       </body>
     </html>
+    </ClerkProvider>
   );
 }
